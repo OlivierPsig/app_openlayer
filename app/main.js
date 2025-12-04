acb20c6e52e1b51bed27dc477e6a07449f5328c8
Olivier Prima <oprima@univ-lyon2.fr>
2025-12-04 11:01:27 +0100

    comit avant de faire une branche

--- app/main.js
+++ app/main.js
@@ -2,13 +2,15 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
+import ImageWMS from 'ol/source/ImageWMS.js';
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    });
   ],
   view: new View({
     center: [0, 0],
