3b978742dbc6396e313426923a01c07f9fe30c41
Olivier Prima <oprima@univ-lyon2.fr>
2025-12-04 11:43:03 +0100

    v1

--- app/main.js
+++ app/main.js
@@ -2,16 +2,30 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import TileWMS from 'ol/source/TileWMS';
+import ImageLayer from 'ol/layer/Image.js';
 import ImageWMS from 'ol/source/ImageWMS.js';
+
+
+const couche_osm =
+  new TileLayer({
+    source: new OSM(),
+  });
+
+const source_dealsbycountry =
+  new ImageWMS({
+      url: 'http://localhost:8080/geoserver/land_matrix/wms',
+      params: {'LAYERS':'land_matrix:deals_by_country'},
+      serverType: 'geoserver',
+  });
+
+const couche_dealsbycountry =
+  new ImageLayer({
+    source : source_dealsbycountry,
+  });
 
 const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    });
-  ],
+  layers: [couche_osm, couche_dealsbycountry],
   view: new View({
     center: [0, 0],
     zoom: 2
